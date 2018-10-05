f16f9245798bfcc52ccb939c85171e2bafa07c48
Christopher Coleman <mcao2006@gmail.com>
2018-10-05 12:17:47 +0100

    Changed: Readded specific routing file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/About/about.component';
+import { LoginComponent } from './components/Login/login.component';
+import { ControlComponent } from './components/Control/control.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'control', component: ControlComponent},
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: 'login' }
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],    
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
 import { AboutComponent } from './components/About/about.component';
 import { ControlComponent } from './components/Control/control.component';
 import { LoginComponent } from './components/Login/login.component';
 
-const appRoutes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'control', component: ControlComponent},
-  { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: 'login' }
-];
 
 @NgModule({
 
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule    
   ],
 
   declarations: [
